15351779d1aba262abba8416e8db17f228d96a2e
suhavni <suhavni@gmail.com>
2023-01-13 16:05:27 +0700

    updated code to support multiple files

--- exercises/week01/stat_file.py
+++ exercises/week01/stat_file.py
@@ -10,14 +10,24 @@ def get_float(string: str):
 def filter_none(iterable):
     return list(filter(lambda x: x is not None, iterable))
 
-try:
-    file = sys.argv[1]
-    f = open(file, "r")
-    nums = np.array(filter_none([get_float(line) for line in f.readlines()]))
-except (IndexError, FileNotFoundError):
-    nums = np.array([])
+def print_summary(nums):
+    print("mean: ", None if len(nums) == 0 else np.mean(nums), end=" ")
+    print("std: ", None if len(nums) == 0 else np.std(nums), end=" ")
+    print("min: ", None if len(nums) == 0 else np.min(nums), end=" ")
+    print("max: ", None if len(nums) == 0 else np.max(nums))
 
-print("mean: ", None if len(nums) == 0 else np.mean(nums))
-print("std: ", None if len(nums) == 0 else np.std(nums))
-print("min: ", None if len(nums) == 0 else np.min(nums))
-print("max: ", None if len(nums) == 0 else np.max(nums))
+def get_file_summary(file_name):
+    try:
+        f = open(file_name, "r")
+        nums = np.array(filter_none([get_float(line) for line in f.readlines()]))
+    except (IndexError, FileNotFoundError):
+        nums = np.array([])
+    finally:
+        f.close()
+    print_summary(nums)
+    return nums
+
+print("Statistics Summary")
+total = [get_file_summary(file) for file in sys.argv[1:]]
+print("combined:")
+print_summary(np.concatenate(total))
